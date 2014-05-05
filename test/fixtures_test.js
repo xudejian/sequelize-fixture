43d743c9e87aa67536221063f5e072a4d6ffcf8b
Dejian Xu <xudejian2008@gmail.com>
2014-05-05 11:11:13 +0800

    should generate different fixtures everytime

--- test/fixtures_test.js
+++ test/fixtures_test.js
@@ -7,11 +7,21 @@ var Sequelize = require('sequelize'),
 describe('.fixtures', function () {
   'use strict';
 
-  var Foo = sequelize.define('Foo', { });
+  var Foo = sequelize.define('Foo', {
+    title: Sequelize.STRING
+  });
 
-  describe('Sequelize.prototype.fixtures', function () {
+  describe('bind in every model', function () {
     it('should can access fixtures in sequelize models', function () {
       assert.equal(typeof Foo.fixtures, 'function');
+    });
+  });
+
+  describe('generate random fixtures', function () {
+    it('should generate different fixtures everytime', function () {
+      var prev = Foo.fixtures(),
+        curr = Foo.fixtures();
+      assert.notDeepEqual(prev, curr);
     });
   });
 
