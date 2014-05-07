518856882487ff43d0ad6b7fd36af1c37f8740f0
Dejian Xu <xudejian2008@gmail.com>
2014-05-07 15:50:41 +0800

    strict test

--- test/model_test.js
+++ test/model_test.js
@@ -94,22 +94,75 @@ describe('Models', function () {
       User = sequelize.define('User', { });
       Project = sequelize.define('Project', { });
       Task = sequelize.define('Task', { });
+      User.hasOne(Project);
       Project.hasMany(Task);
       Task.belongsTo(User);
+      User.hasMany(Task);
 
       sequelize.sync({force:true}).success(function() {
         done();
       });
     });
 
-    it('should create associate data given options.include', function (done) {
+    it('should support hasMany', function (done) {
       var fixture = Project.fixtures({
         include: [{
           model: Task
         }]
       }).success(function(projects) {
         for (var i in projects) {
-          assert.ok(projects[i].tasks);
+          var project = projects[i];
+          assert.ok(project.tasks);
+          for (var j in project.tasks) {
+            assert.equal(project.tasks[j].ProjectId, project.id);
+          }
+        }
+        done();
+      });
+    });
+
+    it('should support hasOne', function (done) {
+      var fixture = User.fixtures({
+        include: [{
+          model: Project
+        }]
+      }).success(function(users) {
+        for (var i in users) {
+          var user = users[i];
+          assert.ok(user.project);
+          assert.equal(user.project.UserId, user.id);
+        }
+        done();
+      });
+    });
+
+    it('should support belongsTo', function (done) {
+      var fixture = Task.fixtures({
+        include: [{
+          model: User
+        }]
+      }).success(function(tasks) {
+        for (var i in tasks) {
+          var task = tasks[i];
+          assert.ok(task.user);
+          assert.equal(task.UserId, task.user.id);
+        }
+        done();
+      });
+    });
+
+    it('should support nested associate', function (done) {
+      var fixture = User.fixtures({
+        include: [{
+          model: Project,
+          include: [{
+            model: Task
+          }]
+        }]
+      }).success(function(users) {
+        for (var i in users) {
+          var user = users[i];
+          assert.ok(user.project.tasks);
         }
         done();
       });
